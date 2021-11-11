e923a9ff63b7f0ef3fda5e772ef977cbc5351b49
spanghf37 <spanghf37@gmail.com>
2021-11-11 15:35:26 +0100

    Update openshift-rhacm-provisioner.sh

--- openshift-sno/scripts/openshift-rhacm-provisioner.sh
+++ openshift-sno/scripts/openshift-rhacm-provisioner.sh
@@ -4,8 +4,8 @@
 
 # Create a non-root user (user-name) and provide that user with sudo privileges by running the following commands:
 useradd kni
-passwd <password>
-echo "kni ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/<user-name>
+passwd
+echo "kni ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/kni
 chmod 0440 /etc/sudoers.d/kni
 
 su - kni -c "ssh-keygen -t rsa -f /home/kni/.ssh/id_rsa -N ''"
@@ -35,23 +35,22 @@ sudo virsh pool-start default
 sudo virsh pool-autostart default
 
 # View the followig examples to configure networking - Provisioning Network (IPv4 address):
-export PROV_CONN=
+export PROV_CONN=enp1s0
 sudo nohup bash -c """
     nmcli con down "$PROV_CONN"
     nmcli con delete "$PROV_CONN"
     nmcli connection add ifname provisioning type bridge con-name provisioning
-    nmcli con add type bridge-worker ifname "$PROV_CONN" master provisioning
+    nmcli con add type bridge-slave ifname "$PROV_CONN" master provisioning
     nmcli connection modify provisioning ipv4.addresses 172.22.0.1/24 ipv4.method manual
     nmcli con down provisioning
     nmcli con up provisioning"""
 
-export PUB_CONN=
-
+export PUB_CONN=enp7s0
 sudo nohup bash -c """
     nmcli con down "$PUB_CONN"
     nmcli con delete "$PUB_CONN"
     nmcli connection add ifname baremetal type bridge con-name baremetal
-    nmcli con add type bridge-worker ifname "$PUB_CONN" master baremetal
+    nmcli con add type bridge-slave ifname "$PUB_CONN" master baremetal
     nmcli connection modify baremetal ipv4.addresses 192.168.122.52/24 ipv4.dns 192.168.122.1 ipv4.method manual
     nmcli con down baremetal
     nmcli con up baremetal"""
